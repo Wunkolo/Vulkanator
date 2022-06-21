f9ba212fd14b1591a18b278f39e07427b51c84e8
Wunkolo <Wunkolo@gmail.com>
2022-06-20 17:37:11 -0700

    Run clang-format

--- include/VulkanConfig.hpp
+++ include/VulkanConfig.hpp
@@ -1,7 +1,9 @@
 #pragma once
 
-// Override preprocessor definitions like `VULKAN_HPP_ASSERT` to provide vulkan-hpp your own assertion function
+// Override preprocessor definitions like `VULKAN_HPP_ASSERT` to provide
+// vulkan-hpp your own assertion function
 
-// Rather than using exceptions, we use error code results so we can specially-handle specific types of vulkan errors;
+// Rather than using exceptions, we use error code results so we can
+// specially-handle specific types of vulkan errors;
 #define VULKAN_HPP_NO_EXCEPTIONS
 #include <vulkan/vulkan.hpp>
